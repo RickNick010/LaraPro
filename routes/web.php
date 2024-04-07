fa07eff50749f4f2091226d52757a5df95785685
RickNick010 <knuipisrichard@gmail.com>
2024-04-07 19:54:55 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', 
+    [
+        'listings' => Listing::all()
+    ]);
 });
+
+Route::get('/listings/{id}', function($id)
+{
+    return view('listing',
+    [
+        'listing' => Listing::find($id)
+    ]);
+});
+
+
